c0a7214cfdcfcf187aba4799cab28af4b3deca10
Smile <smailbestybay@gmail.com>
2024-06-08 16:34:33 -0700

    combine nav into header component, edit marging

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
-import Nav from "./Nav";
-
 export default function Header() {
   return (
     <>
-      <div className="m-4 flex flex-col space-y-1 sm:flex-row sm:justify-between">
+      <div className="m-4 flex flex-col sm:flex-row sm:justify-between">
         <div>Justin HB Schwartz</div>
-        <Nav />
+        <nav>
+          <ol className="mt-2 flex flex-col sm:mt-0 sm:flex-row sm:space-x-10">
+            <li>About</li>
+            <li>Expertise</li>
+            <li>Achievements</li>
+            <li>Work Samples</li>
+          </ol>
+        </nav>
       </div>
     </>
   );
