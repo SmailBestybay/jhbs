5e210b6845402be885d7e055b8eb96aee01605a2
Smile <smailbestybay@gmail.com>
2024-06-10 23:35:17 -0700

    fix file name type

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Header from "./components/Header";
 import About from "./components/About";
 import Expertise from "./components/Expertise";
-import Achievements from "./components/Achievments";
+import Achievements from "./components/Achievements";
 import WorkSamples from "./components/WorkSample";
 import Footer from "./components/Footer";
+
 function App() {
   return (
     <div className="mx-4 flex max-w-[1200px] flex-col gap-2 sm:mx-auto sm:px-2">
