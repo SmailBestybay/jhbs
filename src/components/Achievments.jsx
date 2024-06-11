f189f4854607109b8ed61195f86f7a13e1924974
Smile <smailbestybay@gmail.com>
2024-06-10 23:19:30 -0700

    add number headings to achievments, cleanup worksample

--- src/components/Achievments.jsx
+++ src/components/Achievments.jsx
@@ -1,19 +1,31 @@
 export default function Achievements() {
   return (
-    <section className="grid sm:grid-cols-4">
+    <section className="grid gap-6 sm:grid-cols-4">
       <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis,
-        quam?
+        <h2 className="text-center">420</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis,
+          quam?
+        </p>
       </div>
       <div>
-        Adipisci impedit voluptatibus repudiandae placeat, itaque minima animi
-        libero fugit?
+        <h2 className="text-center">1</h2>
+        <p>
+          Adipisci impedit voluptatibus repudiandae placeat, itaque minima animi
+          libero fugit?
+        </p>
       </div>
       <div>
-        Ad culpa architecto ea odit facilis quibusdam. Officiis, ducimus!
-        Placeat!
+        <h2 className="text-center">69</h2>
+        <p>
+          Ad culpa architecto ea odit facilis quibusdam. Officiis, ducimus!
+          Placeat!
+        </p>
       </div>
-      <div>Possimus magni illo a sint vel ex veritatis fuga nam!</div>
+      <div>
+        <h2 className="text-center">1000</h2>
+        <p>Possimus magni illo a sint vel ex veritatis fuga nam!</p>
+      </div>
     </section>
   );
 }
--- src/components/WorkSample.jsx
+++ src/components/WorkSample.jsx
@@ -1,11 +1,9 @@
-// import PropTypes from "prop-types";
-
 export default function WorkSamples() {
   return (
     <section className="grid grid-cols-1 gap-2 sm:grid-cols-2">
       <div className="bg-sky-400">
         <img
-          className="object-[100%_30%] h-24 w-full object-cover pt-2"
+          className="h-24 w-full object-cover object-[100%_30%] pt-2"
           src="./sample.jpg"
           alt="sample"
         />
@@ -49,55 +47,4 @@ export default function WorkSamples() {
       </div>
     </section>
   );
-}
-
-// function Card(props) {
-//   return (
-//     <div className={props.className ? props.className : ""}>
-//       <img className="h-24 w-96 object-cover" src={props.img} alt={props.alt} />
-//       <h2>{props.headingText}</h2>
-//       <p>{props.paragraph}</p>
-//     </div>
-//   );
-// }
-
-// Card.propTypes = {
-//   img: PropTypes.string,
-//   alt: PropTypes.string,
-//   headingText: PropTypes.string,
-//   paragraph: PropTypes.string,
-//   className: PropTypes.string,
-// };
-
-{
-  /* <Card
-        className="bg-amber-200"
-        img="./sample.jpg"
-        alt="sample text"
-        headingText="Heading Text"
-        paragraph="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, numquam.
-      "
-      />
-      <Card
-        img="./sample.jpg"
-        alt="sample text"
-        headingText="Heading Text"
-        paragraph="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, numquam.
-      "
-      />
-      <Card
-        img="./sample.jpg"
-        alt="sample text"
-        headingText="Heading Text"
-        paragraph="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, numquam.
-      "
-      />
-      <Card
-        className="bg-slate-400"
-        img="./sample.jpg"
-        alt="sample text"
-        headingText="Heading Text"
-        paragraph="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, numquam.
-      "
-      /> */
 }
