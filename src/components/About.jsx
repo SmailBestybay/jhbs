fc0ea961f766605638e011b7b2aa3603dbac79c7
Smile <smailbestybay@gmail.com>
2024-06-10 23:39:41 -0700

    adjjust img width in about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,14 @@
 import sampleImg from "../assets/sample.jpg";
 export default function About() {
   return (
-    <section className="grid sm:grid-cols-2 bg-amber-400">
+    <section className="grid bg-amber-400 sm:grid-cols-2">
       <div className="my-2 flex flex-col gap-2">
         <h1>Hi there, I am Justin</h1>
         <p>Lorem ipsum dolor sit amet.</p>
       </div>
       <div className="sm:flex sm:justify-end">
         <img
-          className="max-w-52 sm:max-w-80"
+          className="max-w-full sm:max-w-96"
           src={sampleImg}
           alt="Sample image"
         />
