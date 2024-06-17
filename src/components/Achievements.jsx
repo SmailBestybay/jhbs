87f05955678ffbe688972329ef852e4089c87416
Smile <smailbestybay@gmail.com>
2024-06-17 00:44:50 -0700

    add achievments number colors and breakpoints

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -2,31 +2,41 @@ export default function Achievements() {
   return (
     <section>
       <h2 className="text-2xl">Achievements</h2>
-      <div className="grid gap-6 sm:grid-cols-4">
+      <div className="grid gap-6 px-6 sm:grid-cols-4">
         <div>
-          <h3 className="text-center">420</h3>
-          <p>
+          <h3 className="my-6 text-center text-2xl font-bold text-sky-600">
+            420
+          </h3>
+          <p className="text-xl font-light">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis,
             quam?
           </p>
         </div>
         <div>
-          <h3 className="text-center">1</h3>
-          <p>
+          <h3 className="my-6 text-center text-2xl font-bold text-sky-600">
+            1
+          </h3>
+          <p className="text-xl font-light">
             Adipisci impedit voluptatibus repudiandae placeat, itaque minima
             animi libero fugit?
           </p>
         </div>
         <div>
-          <h3 className="text-center">69</h3>
-          <p>
+          <h3 className="my-6 text-center text-2xl font-bold text-sky-600">
+            69
+          </h3>
+          <p className="text-xl font-light">
             Ad culpa architecto ea odit facilis quibusdam. Officiis, ducimus!
             Placeat!
           </p>
         </div>
         <div>
-          <h3 className="text-center">1000</h3>
-          <p>Possimus magni illo a sint vel ex veritatis fuga nam!</p>
+          <h3 className="my-6 text-center text-2xl font-bold text-sky-600">
+            1000
+          </h3>
+          <p className="text-xl font-light">
+            Possimus magni illo a sint vel ex veritatis fuga nam!
+          </p>
         </div>
       </div>
     </section>
