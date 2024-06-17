30d705885aa0ee2184308352c441ea14e92b36e3
Smile <smailbestybay@gmail.com>
2024-06-16 21:31:26 -0700

    add breakpoint to expertise width

--- src/components/Expertise.jsx
+++ src/components/Expertise.jsx
@@ -7,7 +7,7 @@ export default function Expertise() {
           <h3 className="text-2xl font-semibold before:mr-5 before:content-['01']">
             Brand Identity
           </h3>
-          <p className="ml-12 mt-2 w-32">
+          <p className="ml-12 mt-2 sm:w-32">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
             consequuntur.
           </p>
@@ -16,7 +16,7 @@ export default function Expertise() {
           <h3 className="text-2xl font-semibold before:mr-5 before:content-['02']">
             Web & App
           </h3>
-          <p className="ml-12 mt-2 w-32">
+          <p className="ml-12 mt-2 sm:w-32">
             A, sint neque. Commodi quisquam officia aliquid temporibus
             voluptatum corrupti!
           </p>
@@ -25,7 +25,7 @@ export default function Expertise() {
           <h3 className="text-2xl font-semibold before:mr-5 before:content-['03']">
             Print Design
           </h3>
-          <p className="ml-12 mt-2 w-32">
+          <p className="ml-12 mt-2 sm:w-32">
             Explicabo optio odit modi quam pariatur officia quis dolore
             laudantium!
           </p>
