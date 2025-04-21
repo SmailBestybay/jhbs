309046e45c2560f2073a2a637c324fbf3d162076
Smile <smailbestybay@gmail.com>
2025-04-20 22:06:42 -0700

    add github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://smailbestybay.github.io/',
+  base: 'jhbs',
   vite: {
     plugins: [tailwindcss()]
   }
