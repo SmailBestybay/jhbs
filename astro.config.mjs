059ceb04640436fa4330cf1a6594d99ec92352da
Smile <smailbestybay@gmail.com>
2025-04-22 22:30:44 -0700

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://smailbestybay.github.io/',
   base: 'jhbs',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sitemap()]
 });
